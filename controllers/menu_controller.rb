a2164e2087c8dc6e2b5990d008597e578e64eda9
atsantiago <atsantiago089@gmail.com>
2018-10-14 13:52:54 -0600

    Assignment 05 menu: completed

--- controllers/menu_controller.rb
+++ controllers/menu_controller.rb
@@ -10,10 +10,11 @@ class MenuController
   def main_menu
     puts "Main Menu - #{address_book.entries.count} entries"
     puts "1 - View all entries"
-    puts "2 - Create an entry"
-    puts "3 - Search for an entry"
-    puts "4 - Import entries from a CSV"
-    puts "5 - Exit"
+    puts "2 - View Entry Number n"
+    puts "3 - Create an entry"
+    puts "4 - Search for an entry"
+    puts "5 - Import entries from a CSV"
+    puts "6 - Exit"
     print "Enter your selection: "
 
     selection = gets.to_i
@@ -25,17 +26,21 @@ class MenuController
       main_menu
     when 2
       system "clear"
-      create_entry
+      view_entry_number
       main_menu
     when 3
       system "clear"
-      search_entries
+      create_entry
       main_menu
     when 4
       system "clear"
-      read_csv
+      search_entries
       main_menu
     when 5
+      system "clear"
+      read_csv
+      main_menu
+    when 6
       puts "Good-bye!"
       exit(0)
     else
@@ -53,6 +58,22 @@ class MenuController
     end
     system "clear"
     puts "End of entries"
+  end
+
+  def view_entry_number
+    puts "Please input the entry number"
+    n = gets.chomp
+    system "clear"
+    if n.to_i > address_book.entries.size || n.to_i <= 0
+      system "clear"
+      puts "#{n.to_s} is not a valid input"
+    end
+
+    address_book.entries.each_with_index do |entry, index|
+      next if index != (n.to_i - 1)
+        puts "Entry number #{n.to_s}"
+        puts entry.to_s
+    end
   end
 
   def create_entry
